111789d71c38156bb409ef7bcfad19e77d125fc6
barathwajgit <78818645+barathwajgit@users.noreply.github.com>
2022-09-02 15:15:30 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        int x = input.nextInt();
+        input.close();
+        System.out.println(x);
+    }
+
+}
